505dc1088d5ebb44fcf682caa15ea412aaaad987
Changwalker <Changwalker@users.noreply.github.com>
2021-03-05 18:23:10 +0800

    20210305

---
+++ 326.3-的幂.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=326 lang=cpp
+ *
+ * [326] 3的幂
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n < 1)
+            return false;
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        
+        return n == 1;
+    }
+};
+// @lc code=end
+
