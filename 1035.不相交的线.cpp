e7561cd349b77bd4053e70a1ab21e73959d1f92b
Changwalker <511260497@qq.com>
2021-05-21 14:23:00 +0800

    20210521

---
+++ 1035.不相交的线.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1035 lang=cpp
+ *
+ * [1035] 不相交的线
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for(int i = 1; i <= n1; ++i){
+            for(int j = 1; j <= n2; ++j){
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if(nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+            }
+        }
+        return dp[n1][n2];
+    }
+};
+// @lc code=end
+
