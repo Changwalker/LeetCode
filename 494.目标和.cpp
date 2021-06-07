6ee878a5212aa4faa300b8c2d428a6fdb90f4e15
Changwalker <511260497@qq.com>
2021-06-07 13:38:04 +0800

    20210607

--- 494.目标和.cpp
+++ 494.目标和.cpp
@@ -11,20 +11,20 @@ using namespace std;
 class Solution {
 public:
     int count = 0;
-    
+
     int findTargetSumWays(vector<int>& nums, int target) {
         backtrack(nums, target, 0, 0);
         return count;
     }
-    void backtrack(vector<int> &nums, int target, int index, int sum){
-        if(index == nums.size()){
-            if(sum == target){
+
+    void backtrack(vector<int>& nums, int target, int index, int sum) {
+        if (index == nums.size()) {
+            if (sum == target) {
                 count++;
             }
-        }
-        else{
+        } else {
             backtrack(nums, target, index + 1, sum + nums[index]);
-            backtrack(nums, target, index - 1, sum - nums[index]);
+            backtrack(nums, target, index + 1, sum - nums[index]);
         }
     }
 };
