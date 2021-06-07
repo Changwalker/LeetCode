6b6f875f5b1f9281c0e9890dd7af844906c780c1
Changwalker <511260497@qq.com>
2021-06-07 13:36:24 +0800

    20210607

---
+++ 494.目标和.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ *
+ * [494] 目标和
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int count = 0;
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+    void backtrack(vector<int> &nums, int target, int index, int sum){
+        if(index == nums.size()){
+            if(sum == target){
+                count++;
+            }
+        }
+        else{
+            backtrack(nums, target, index + 1, sum + nums[index]);
+            backtrack(nums, target, index - 1, sum - nums[index]);
+        }
+    }
+};
+// @lc code=end
+
