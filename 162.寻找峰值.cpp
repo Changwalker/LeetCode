56139d3408daf593918543eee59618a017f43ca9
Changwalker <511260497@qq.com>
2021-09-15 18:21:54 +0800

    20210915

---
+++ 162.寻找峰值.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=cpp
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = (l + r) >> 1  ;
+            if(nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return r;
+    }
+};
+// @lc code=end
+
